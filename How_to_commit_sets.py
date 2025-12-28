4ed52c53a4888ad58e343d5ac527d91b33d14716
xxmrajoxx <andrew.chang2@live.com.au>
2025-12-28 17:11:44 +1100

    Done

--- How_to_commit_sets.py
+++ How_to_commit_sets.py
@@ -4,3 +4,5 @@
 #               -- Add initial python scripts
 # Step 4 - Click the check mark icon to commit the changes
 # Step 5 - Ctrl + s to save the file and then Ctrl + W to close the file
+# Step 6 - Sync changes to remote repository by clicking on the '...' menu and selecting 'Push'
+# Step 7 - Verify changes are reflected in the remote repository on GitHub
