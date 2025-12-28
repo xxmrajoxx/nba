323792b14be9cb11c53f6d70cafad71aa40b44c8
xxmrajoxx <andrew.chang2@live.com.au>
2025-12-28 16:55:59 +1100

    Add initial python scripts

---
+++ How_to_commit_sets.py
@@ -0,0 +1,6 @@
+# Step 1 - Ctrl + Shift + G (source control)
+# Step 2 - Click on + in the 'Changes' section to stage all changes
+# Step 3 - Type commit message in the message box in the first line
+#               -- Add initial python scripts
+# Step 4 - Click the check mark icon to commit the changes
+# Step 5 - Ctrl + s to save the file and then Ctrl + W to close the file
