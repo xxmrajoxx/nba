c4b30eddd3c652b716b72575891f473b5ffd56c9
xxmrajoxx <andrew.chang2@live.com.au>
2026-01-02 19:03:18 +1100

    fix

--- src/extract_players.py
+++ src/extract_players.py
@@ -1,37 +1,110 @@
-from nba_api.stats.endpoints import PlayerCareerStats
+from nba_api.stats.endpoints import playergamelogs
 from nba_api.stats.static import players
 import pandas as pd
+import logging 
+import time
+from typing import Optional, List, Dict
+import os
 
-PLAYER_NAMES = [
-    "Nikola Jokic",
-    "Luka Doncic",
-    "Jayson Tatum"
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+PLAYER_NAMES: List[str] = [
+    "Karl-Anthony Towns",
+    "Domantas Sabonis",
+    "Rudy Gobert",
+    "Ivica Zubac",
+    "Jalen Duren",
+    "Donovan Clingan",
+    "Isaiah Hartenstein",
+    "Jalen Johnson",
+    "Bam Adebayo",
+    "Alperen Sengun",
+    "Andre Drummond",
+    "Josh Giddey",
+    "Evan Mobley",
+    "Deandre Ayton",
+    "Mitchell Robinson",
+    "Steven Adams",
+    "Scottie Barnes",
+    "Alex Sarr",
+    "Mark Williams",
+    "Josh Hart"
 ]
 
-def get_player_id(player_name):
-    nba_players = players.find_players_by_full_name(player_name)
-    if not nba_players:
-        raise ValueError(f"Player not found: {player_name}")
-    return nba_players[0]["id"]
+def get_player_id(player_name: str) -> int:
+    try:
+        logging.info(f"obtaining player id for {player_name}")
+        nba_players = players.find_players_by_full_name(player_name)
 
-def fetch_player_stats(player_name):
-    player_id = get_player_id(player_name)
-    career = PlayerCareerStats(player_id=player_id)
-    df = career.get_data_frames()[0]
-    df["player_name"] = player_name
-    return df
+        if not nba_players:
+            logging.warning(f"Player not found: {player_name}")
+        return None
 
+        return int(nba_players[0]["id"])
+    
+    except Exception:
+        logging.exception(f"Error while looking up player_id for: {player_name}")
+        return None
+ 
+
+def fetch_player_game_logs(player_name: str, season: str, league_id: str="00", season_type: str="Regular Season") -> pd.DataFrame:
+    try:
+        player_id = get_player_id(player_name)
+        if player_id is None:
+            return pd.DataFrame()
+
+        logging.info(f"extracting game logs for {player_name} for {season}")
+
+        endpoint = playergamelogs.PlayerGameLogs(
+            player_id_nullable=player_id,
+            season_nullable=season,
+            league_id_nullable=league_id,
+            season_type_nullable=season_type
+        )
+
+        df = endpoint.player_game_logs.get_data_frame()
+        
+        if df.empty:
+            logging.warning(f"no game logs for {player_name} for {season}")
+            return pd.DataFrame()
+        
+        # Important - Metadata enrichment - unlocks partitioning in S3, Filtering in Bedrock
+        df["player_name"] = player_name
+        df["season"] = season
+        df["player_id"] = player_id
+        df["season_type"] = season_type
+        
+        logging.info(f"sucessfully extracted game log for {player_name} for {season}")
+        return df
+
+    except Exception:
+        logging.exception(f"error extracting game logs for {player_name} for {season}")
+        return pd.DataFrame()
+    
 def main():
-    all_players_df = []
+    SEASON = "2025-26"
+    all_dfs = []
+
+    logging.info(f"commencing nba player game log extraction")
 
     for name in PLAYER_NAMES:
-        print(f"Fetching stats for {name}")
-        df = fetch_player_stats(name)
-        all_players_df.append(df)
+        df = fetch_player_game_logs(player_name=name, season=SEASON)
 
-    final_df = pd.concat(all_players_df, ignore_index=True)
-    final_df.to_csv("nba_player_stats.csv", index=False)
-    print("Saved nba_player_stats.csv")
+        if not df.empty:
+            all_dfs.append(df)
 
-if __name__ == "__main__":
-    main()
+            time.sleep(1)
+
+    if not all_dfs:
+        logging.error(f"no game logs collected for any player")
+        return
+    
+    final_df = pd.concat(all_dfs, ignore_index=True)
+    output_file = f"nba_player_game_logs{SEASON}.csv"
+    final_df.to_csv(output_file, index=False)
+
+    logging.info(f"saved file")
+
+if __name__ =="__main__":
+    main
+
