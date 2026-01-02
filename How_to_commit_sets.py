af6b09f352afd4bcf504c9c2c187ae665ca4dc30
xxmrajoxx <andrew.chang2@live.com.au>
2026-01-02 19:41:38 +1100

    delete

--- How_to_commit_sets.py
+++
@@ -1,8 +0,0 @@
-# Step 1 - Ctrl + Shift + G (source control)
-# Step 2 - Click on + in the 'Changes' section to stage all changes
-# Step 3 - Type commit message in the message box in the first line
-#               -- Add initial python scripts
-# Step 4 - Click the check mark icon to commit the changes
-# Step 5 - Ctrl + s to save the file and then Ctrl + W to close the file
-# Step 6 - Sync changes to remote repository by clicking on the '...' menu and selecting 'Push'
-# Step 7 - Verify changes are reflected in the remote repository on GitHub
